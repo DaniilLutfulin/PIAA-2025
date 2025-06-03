7a145203a007aac2e2adbcec6a835e79e2c403f3
Daniil Lutfulin <alebardavormit@gmail.com>
2025-06-03 03:26:02 +0300

    lb3-quickfix

--- lb3_levenshtein/concatenation.py
+++ lb3_levenshtein/concatenation.py
@@ -1,23 +1,23 @@
 def min_edit_cost(replace_cost, insert_cost, delete_cost, A, B):
     n, m = len(A), len(B)
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    distance_matrix = [[0] * (m + 1) for _ in range(n + 1)]
 
     for i in range(1, n + 1):
-        dp[i][0] = i * delete_cost
+        distance_matrix[i][0] = i * delete_cost
     for j in range(1, m + 1):
-        dp[0][j] = j * insert_cost
+        distance_matrix[0][j] = j * insert_cost
 
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             if A[i - 1] == B[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1]
+                distance_matrix[i][j] = distance_matrix[i - 1][j - 1]
             else:
-                dp[i][j] = min(
-                    dp[i-1][j-1] + replace_cost,  # замена
-                    dp[i][j - 1] + insert_cost,       # вставка
-                    dp[i - 1][j] + delete_cost        # удаление
+                distance_matrix[i][j] = min(
+                    distance_matrix[i-1][j-1] + replace_cost,  # замена
+                    distance_matrix[i][j - 1] + insert_cost,       # вставка
+                    distance_matrix[i - 1][j] + delete_cost        # удаление
                 )
-    return dp
+    return distance_matrix
 
 def min_cost_A_to_BC(replace_cost, insert_cost, delete_cost, A, B, C):
     n = len(A)
@@ -41,13 +41,12 @@ replace_cost, insert_cost, delete_cost = map(int, input().split())
 A = input().strip()
 B = input().strip()
 C = input().strip()
-
-# c = min_cost_A_to_BC(replace_cost, insert_cost, delete_cost, A, B)[-1][-1]
-# if len(B < 1000):
-#     for i in range(0,len(B)+1):
-#         cost = min_cost_A_to_BC(replace_cost,insert_cost,delete_cost,A, B[:i], B[i:])
-#         if cost!=c:
-#             print("Неверное решение")
+# c = min_edit_cost(replace_cost, insert_cost, delete_cost, A, B)[-1][-1]
+# for i in range(0,len(B)+1):
+#     cost = min_cost_A_to_BC(replace_cost,insert_cost,delete_cost,A, B[:i], B[i:])
+#     print(A, B[:i], B[i:], cost)
+#     if cost!=c:
+#         print("Неверное решение")
 
 c = min_cost_A_to_BC(replace_cost, insert_cost, delete_cost, A, B, C)
 print(c)
